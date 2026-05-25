ab0aade7b14b4d4d8334815654a2fab1389457b9
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-05-25 13:36:07 +0600

    Create Variables&DataTypes.cs

---
+++ Learn C# Basic/FirstConsoleApp/Variables&DataTypes.cs
@@ -0,0 +1,11 @@
+class Variables
+{
+    public static void Main(string[] args)
+    {
+        string name;
+        name = "Safin Mahmud";
+        Console.WriteLine(name);
+        Console.WriteLine(name);
+        
+    }
+}

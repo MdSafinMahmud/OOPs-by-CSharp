1282f79bc8ffe187c357b155e2e72da3b31e9291
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-05-25 22:45:55 +0600

    Update user_input.cs

--- Learn C# Basic/FirstConsoleApp/user_input.cs
+++ Learn C# Basic/FirstConsoleApp/user_input.cs
@@ -4,21 +4,22 @@ class userInput
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please input Student information ");
+        Console.WriteLine("Please input your University information: ");
         Console.Write("Enter your name: ");
         string studentName = Console.ReadLine() ?? string.Empty;
         Console.Write("Enter your Age: ");
         int age = int.Parse(Console.ReadLine() ?? "0");
-        Console.Write("Enter your Varsity name: ");
+        Console.Write("Enter your University name: ");
         string varsityName = Console.ReadLine() ?? string.Empty;
         Console.Write("Enter Student ID: ");
         long stuID = long.Parse(Console.ReadLine() ?? "0");
         Console.Write("Enter your Program Name: ");
         string programName = Console.ReadLine() ?? string.Empty;
+        Console.WriteLine("\nHere is your information (Printed copy):");
 
         Console.WriteLine("Student Name: " + studentName);
         Console.WriteLine("Student Age: " + age);
-        Console.WriteLine("Varsity Name: " + varsityName);
+        Console.WriteLine("University Name: " + varsityName);
         Console.WriteLine("Student ID: " + stuID);
         Console.WriteLine("Program Name: " + programName);
     }

d54e64c4854c7e58cb9a7b387876138f098244e9
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-05-25 23:45:50 +0600

    Update MathMaticalOp.cs

--- Learn C# Basic/FirstConsoleApp/MathMaticalOp.cs
+++ Learn C# Basic/FirstConsoleApp/MathMaticalOp.cs
@@ -8,8 +8,14 @@ class MathMatics
         int a = int.Parse(Console.ReadLine() ?? "0");
         int b = int.Parse(Console.ReadLine() ?? "0");
         int c = int.Parse(Console.ReadLine() ?? "0");
-        Console.Write("The result: ");
         int result = a + b + c;
+        Console.WriteLine("The result: " + result);
+        int result1 = a - b * c;
+        Console.WriteLine("The result1: " + result1);
+        int result2 = a + b / c;
+        Console.WriteLine("The result2: " + result2);
+        int result3 = a + b - c;
+        Console.WriteLine("The result3: " + result3);
         Console.WriteLine(result);
     }
 }

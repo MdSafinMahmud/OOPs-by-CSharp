4f0b179efce761c6a91292e8130cd93d3271ebb8
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-05-25 14:26:40 +0600

    Update Variables&DataTypes.cs

--- Learn C# Basic/FirstConsoleApp/Variables&DataTypes.cs
+++ Learn C# Basic/FirstConsoleApp/Variables&DataTypes.cs
@@ -2,10 +2,18 @@ class Variables
 {
     public static void Main(string[] args)
     {
-        string name;
-        name = "Safin Mahmud";
-        Console.WriteLine(name);
-        Console.WriteLine(name);
+        string fullName;
+        int age = 20;
+        double gpa = 3.7;
+        bool isResigterd = true;
+        char bloodGroup = 'O';
+
+        fullName = "Safin Mahmud";
+        Console.WriteLine("Name: "+ fullName);
+        Console.WriteLine("Age: "+ age);
+        Console.WriteLine("GPA: "+ gpa);
+        Console.WriteLine("Resisterd?:" + isResigterd);
+        Console.WriteLine("Blood Group: "+ bloodGroup);
         
     }
 }

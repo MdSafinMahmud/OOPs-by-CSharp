bcca9dd86b6063d80d95c65f4db27dfc5d586c67
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-05-25 15:56:33 +0600

    Update Assignment_2.cs

--- Learn C# Basic/FirstConsoleApp/Assignment_2.cs
+++ Learn C# Basic/FirstConsoleApp/Assignment_2.cs
@@ -5,19 +5,19 @@ class Assignment
         string phoneName;
         double prize;
         string category;
-        bool available;
+        bool isAvailable;
         int sold;
 
         phoneName = "Apple iphone 14";
         prize = 320.5;
         category = "Smartphone";
-        available = true;
+        isAvailable = true;
         sold = 5;
 
         Console.WriteLine("Name: " + phoneName);
         Console.WriteLine("Prize:\t$" + prize);
         Console.WriteLine("Category: " + category);
-        Console.WriteLine("Available: " + available);
+        Console.WriteLine("isAvailable: " + isAvailable);
         Console.WriteLine("Sold: " + sold);
 
 

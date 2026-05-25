1a78c80f1307499d6704f6e410e8a33c09a85f78
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-05-25 16:17:53 +0600

    Create TypeCasting.cs

---
+++ Learn C# Basic/FirstConsoleApp/TypeCasting.cs
@@ -0,0 +1,12 @@
+class FirstConsoleApp
+{
+    public static void Main(string[] args)
+    {
+        double salary = 2549.67;
+        Console.WriteLine(salary);
+
+        int salary2 = (int)salary;
+        Console.WriteLine(salary2);
+        Console.WriteLine(Convert.ToString(salary));
+    }
+}

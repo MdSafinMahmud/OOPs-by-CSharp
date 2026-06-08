9448fb9dbec31f5ae4d18259e3173b6e240d17b0
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-06-09 00:07:07 +0600

    Create LabTask_1_3.cs

---
+++ Learn C# Basic/Varsity CLasses/LabTask_1_3.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 20;
+        Console.WriteLine($"Before Swap: a={a},b={b}");
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        Console.Write($"After swap: a={a},b={b}");
+    }
+}

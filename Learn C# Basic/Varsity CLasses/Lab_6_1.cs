07b9be2e083a2de1752991f9700de5f3d480085e
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-06-12 22:03:26 +0600

    Update Lab_6_1.cs

--- Learn C# Basic/Varsity CLasses/Lab_6_1.cs
+++ Learn C# Basic/Varsity CLasses/Lab_6_1.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main()
     {
-        
+        int size=5;
+        int[] numbers = { 10, 20, 30, 40, 50 };
+        Console.WriteLine("Array Elements are: ");
+        for (int i = 0; i < size; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
     }
 }

b2e28412e96ec7fa4c6157f4e66378a963e4edc7
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-06-15 23:32:22 +0600

    Update Lab_6_4.cs

--- Learn C# Basic/Varsity CLasses/Lab_6_4.cs
+++ Learn C# Basic/Varsity CLasses/Lab_6_4.cs
@@ -3,10 +3,10 @@ class Program
 {
     static void Main()
     {
-        int[] arr = { 1, 2, 3, 2, 4, 5, 1, 6 };
-        int size = 7;
+        int[] arr = { 1, 2, 3, 2, 4, 5, 1, 6, 4 };
+        int size = 9;
         Console.WriteLine("Duplicate elements are: ");
-        for(int i = 0; i < size; i++)
+        for (int i = 0; i < size; i++)
         {
             for (int j = i + 1; j < size; j++)
             {

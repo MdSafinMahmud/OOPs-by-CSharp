6643db0dcccb91311dccc32d04dd2a2b740409fb
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-08-15 00:58:10 +0600

    Create 04_Methods.cs

---
+++ OOP/04_Methods.cs
@@ -0,0 +1,23 @@
+using System;
+
+class BankAccount
+{
+    public double balance;
+    public string name = "";
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Account Holder:{name}");
+        Console.WriteLine($"Account Balance:{balance}");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount myAccount = new();
+        myAccount.balance = 5000;
+        myAccount.name = "Safin Mahmud";
+        myAccount.DisplayInfo();
+    }
+}
